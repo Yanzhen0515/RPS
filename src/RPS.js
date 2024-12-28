6f6c3504f398765e9a172c1ef7865f74f8d7b7d9
Yanzhen0515 <97456258+Yanzhen0515@users.noreply.github.com>
2024-12-27 19:13:16 -0500

    system wrong, code should work

--- src/RPS.js
+++ src/RPS.js
@@ -1,42 +1,33 @@
-
-
-//check for player input
 module.exports = function(player) {
-    if (['Rock', 'Scissor', 'Paper'].indexOf(player) == -1){
+    if (['Rock', 'Scissor', 'Paper'].indexOf(player) == -1) {
         return { code: -1, message: 'have a valid input!' };
     }
 
     // a random generator for RPS
-    var random = Math.random(); //generate a float number between 0.0 to 1.0
+    var random = Math.random(); // generate a float number between 0.0 to 1.0
     var Computer;
 
     // random generate for computer action
-    if(random <= 0.33){
+    if (random <= 0.33) {
         Computer = "Rock";
-    }
-    else if(random > 0.67){
-        Computer = "scissor";
-    }
-    else{
+    } else if (random > 0.67) {
+        Computer = "Scissor";
+    } else {
         Computer = 'Paper';
     }
 
-    check(player, Computer);
-
+    return check(player, Computer); // Return the result of check
 }
 
-//check for who is win
-var check = function(Player, Computer){
-    if(Computer == Player){
-        return 0;
+//check for who wins
+var check = function(Player, Computer) {
+    if (Computer == Player) {
+        return 0; // Draw
+    } else if ((Computer == 'Rock' && Player == 'Paper') ||
+               (Computer == 'Scissor' && Player == 'Rock') ||
+               (Computer == 'Paper' && Player == 'Scissor')) {
+        return 1; // Player wins
+    } else {
+        return -1; // Player loses
     }
-    else if( (Computer == 'Rock' && Player == 'Paper')
-    || (Computer == 'Scissor' && Player == 'Rock')
-    || (Computer == 'Paper' && Player == 'Scissor')){
-        return 1;
-    }
-    else{
-        return -1;
-    }
-    
 }
--- src/Server.js
+++ src/Server.js
@@ -1,42 +1,44 @@
-
 const http = require('http');
-const fs = require('fs');
 const url = require('url'); 
 const querystring = require('querystring'); 
+const RPS = require('C:/Users/xianyu/Desktop/Project/RPS/src/RPS.');
 
-const RPS = require('./RPS');
 let won = 0;
 
-http.createServer(function(request,response){
-  const par = url.parse(request.url);
+http.createServer(function(request, response) {
+    const par = url.parse(request.url);
 
-  if (par.pathname == '/game') {
-    const query = querystring.parse(parsedUrl.query);
-    
-    const playerAction = query.action;
+    if (par.pathname === '/game') {
+        const query = querystring.parse(par.query);
+        const playerAction = query.action;
 
-    if (won >= 3) {
-      response.writeHead(500);
-      response.end('GG');
-      return
+        if (!playerAction) {
+            response.writeHead(400);
+            response.end('Invalid input');
+            return;
+        }
+
+        if (won >= 3) {
+            response.writeHead(500);
+            response.end('GG');
+            return;
+        }
+
+        const gameResult = RPS(playerAction);
+
+        response.writeHead(200);
+
+        if (gameResult === 0) {
+            response.end('draw');
+        } else if (gameResult === 1) {
+            response.end('win');
+            won++;
+        } else if (gameResult === -1) {
+            response.end('lose');
+        }
+        return;
     }
-  }
-  const game = RPS(playerAction);
-  response.writeHead(200);
 
-  if(game == 0){
-    response.end('draw');
-  }
-
-  else if(game == 1){
-    response.end('win');
-    won++;
-  }
-
-  else if(game == -1){
-    response.end('win');
-  }
-
-
-}
-).listen(8080);
+    response.writeHead(404);
+    response.end('Not Found');
+}).listen(8080);
